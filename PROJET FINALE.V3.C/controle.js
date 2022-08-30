e343eae990493048ba77f423782938e59195d093
Mohammed Benaaouinate <101942955+Mohammedbenaaouinate@users.noreply.github.com>
2022-08-30 12:03:55 +0100

    Add files via upload

---
+++ PROJET FINALE.V3.C/controle.js
@@ -0,0 +1,21 @@
+var input1=document.getElementById("one");
+var input2=document.getElementById("two");
+var input3=document.getElementById("three");
+var input4=document.getElementById("four");
+var erreur="Veillez Remplir tous les champs";
+if(input1==null)
+{
+    document.getElementById("parainput").innerHTML=erreur;
+}
+if(input2==null)
+{
+    document.getElementById("parainput").innerHTML=erreur;
+}
+if(input3==null)
+{
+    document.getElementById("parainput").innerHTML=erreur;
+}
+if(input4==null)
+{
+    document.getElementById("parainput").innerHTML=erreur;
+}
